173f74a056c7db90e96a61949564899c26d6ef5c
Firedynex <turjoy10@gmail.com>
2023-03-08 20:40:29 -0500

    Created a Bash scipt to make compiling classes easier

---
+++ ChessCompileScript.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+javac -d bin src/PermanentCharacteristics/Color.java
+javac -d bin src/PermanentCharacteristics/PieceType.java
+javac -d bin -cp bin src/ChessPieces/ChessPiece.java
+javac -d bin -cp bin src/ChessPieces/King.java
+javac -d bin -cp bin src/ChessPieces/Queen.java
+javac -d bin -cp bin src/ChessPieces/Bishop.java
+javac -d bin -cp bin src/ChessPieces/Knight.java
+javac -d bin -cp bin src/ChessPieces/Rook.java
+javac -d bin -cp bin src/ChessPieces/Pawn.java
+javac -d bin -cp bin src/ChessGame.java
+javac -d bin -cp bin src/ChessDriver.java
